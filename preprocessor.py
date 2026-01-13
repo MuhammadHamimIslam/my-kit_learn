8d9051e9162afecec2998f642b371dfe5a7336c6
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2026-01-13 08:10:48 +0600

    update

---
+++ preprocessor.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+class StandardScaler:
+    """
+    ...
+    """
+    def __init__(self):
+        self.mean = None
+        self.std = None
+    
+    def fit(self, X):
+        self.mean = np.mean(X, axis=0)
+        self.std = np.std(X, axis=0)
+        if (self.std == 0).any():
+            warnings.warn("Your all values are constant. So std is 0")
+    
+    def transform(self, X):
+        if self.mean is None or self.std is None:
+            raise ValueError("Scaler not fitted yet! Call fit or fit_transform first.")
+        return (X - self.mean) / self.std
+    
+    def fit_transform(self, X):
+        self.mean = np.mean(X, axis=0)
+        self.std = np.std(X, axis=0)
+        if (self.std == 0).any():
+            warnings.warn("Your all values are constant. So std is 0")
+
+        return (X - self.mean) / self.std
+    
+    def inverse_transform(self, scaled):
+        if self.mean is None or self.std is None:
+            raise ValueError("Scaler not fitted yet! Call fit or fit_transform first.")
+        return (scaled * self.std) + self.mean
+
+class MinMaxScaler:
+    def __init__(self):
+        self.max = None
+        self.min = None
+    
+    def fit(self, X):
+        self.max = np.max(X, axis=0)
+        self.min = np.min(X, axis=0)
+        
+    def transform(self, X):
+        if self.max is None or self.min is None:
+            raise ValueError("Scaler not fitted yet! Call fit or fit_transform first.")
+        return (X - self.min) / (self.max - self.min)
+    
+    def fit_transform(self, X):
+        self.max = np.max(X, axis=0)
+        self.min = np.min(X, axis=0)
+        
+        return (X - self.min) / (self.max - self.min)
+    
+    def inverse_transform(self, scaled):
+        if self.max is None or self.min is None:
+            raise ValueError("Scaler not fitted yet! Call fit or fit_transform first.")
+        return scaled * (self.max - self.min) + self.min
+
+class OneHotEncoder:
+    def __init__(self):
+        self.categories = None
+
+    def fit(self, X):
+        self.categories = np.sort(np.unique(X))
+    
+    def transform(self, X):
+        if self.categories is None:
+            raise ValueError("Not Fitted yet")
+
+        result = np.zeros((len(X), len(self.categories)))
+        for i, cat in enumerate(self.categories):
+            result[:, i] = (X == cat).astype(int)  # 1 where match, 0 else
+        return result
+    
+    def fit_transform(self, X):
+        self.categories = np.sort(np.unique(X))
+        result = np.zeros((len(X), len(self.categories)))
+        for i, cat in enumerate(self.categories):
+            result[:, i] = (X == cat).astype(int)  # 1 where match, 0 else
+        return result
+
+    def get_features_names(self):
+        if self.categories is None:
+            raise ValueError("Not Fitted yet")
+        return [f"is_{cat}" for cat in self.categories]
+
+class LabelEncoder:
+    def __init__(self):
+        self.classes = None
+    
+    def fit(self, X):
+        self.classes = np.sort(np.unique(X))
+    
+    def transform(self, X):
+        if self.classes is None:
+            raise ValueError("Not Fitted yet")
+        invalid = ~np.isin(X, self.classes)
+        if np.any(invalid):
+            raise ValueError(f"Unknown categories: {np.unique(X[invalid])}")
+        return np.searchsorted(self.classes, X)
+    
+    def fit_transform(self, X):
+        self.classes = np.sort(np.unique(X))
+        return np.searchsorted(self.classes, X)
+    
+    def inverse_transform(self, num):
+        if self.classes is None:
+            raise ValueError("Not Fitted yet")
+        return self.classes[num]
